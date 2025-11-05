d7fb2a20e59317c1b6fccac6adff82bf583374b8
Sujin Hwang <sujinhwang000@gmail.com>
2025-11-05 16:08:30 -0600

    Implemented functions for full dct and dominant dct coefficients analysis

--- src/models/dct/dct_utils.py
+++ src/models/dct/dct_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.fftpack import dct, idct
+import os
 
 def dct_high_freq(data, n_coefficients=2):
     """
@@ -27,10 +28,47 @@ def dct_high_freq(data, n_coefficients=2):
         print(f"DCT computation failed: {e}")
         raise
 
+def dct_full(data):
+    try:
+        # Apply DCT along the row
+        dct_data = dct(data, type=2, norm='ortho', axis=1)
+
+        return dct_data
+
+    except Exception as e:
+        print(f"DCT computation failed: {e}")
+        raise
+
+def dct_dominant(data, n_coefficients=2):
+    """
+    Extracts the first k coefficients from Discrete Cosine Transform
+    for each row of the input 2D numpy array.
+
+    Args:
+        data (np.ndarray): 2D array of size n x m (n rows, m features)
+        n_coefficients (int): number of dominant coefficients to extract
+
+    Returns:
+        np.ndarray: 2D array of size n x k containing the last k DCT coefficients
+    """
+    try:
+
+        # Apply DCT along each row
+        dct_data = dct(data, type=2, norm='ortho', axis=1)
+
+        # Extract the last n_coefficients (high-frequency)
+        dominant = dct_data[:, :n_coefficients]
+
+        return dominant
+
+    except Exception as e:
+        print(f"DCT computation failed: {e}")
+        raise
+
 
 def save_reduced_data(X_reduced, y, out_base):
     """
-    Saves reduced 2D data by class
+    Saves reduced data by class
     """
     for i in np.unique(y):
         class_data = X_reduced[y == i]
